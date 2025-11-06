2f5120d421296610241897506c5edad96b826ea0
Athaa <athanaya.sakhy1@fiwa.sch.id>
2025-11-06 20:16:42 +0700

    Update testing.c

--- testing.c
+++ testing.c
@@ -5,5 +5,17 @@ int main() {
  printf("2");
  printf("3");
  printf("4");
+ printf("5");
+ printf("6");
+ printf("7");
+ printf("8");
+ printf("9");
+ printf("10");
+ printf("11");
+ printf("12");
+ printf("13");
+ printf("14");
+ printf("15");
+ printf("16");
  return 0;
 }
